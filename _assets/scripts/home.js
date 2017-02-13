459244dab047c998b0ec39f4f86950d89c32fed2
dakshgautam <daksh.gautam@hashedin.in>
2017-02-13 11:29:15 +0530

    Milestone 4 & 5

--- _assets/scripts/home.js
+++ _assets/scripts/home.js
@@ -1,1 +1,23 @@
 //Write JS logic here
+
+// $(".fa").click(function() {
+//   alert("hello world !");
+//   $(".dropdown").css ({
+//     "display" :"flex",
+//     "flex-direction":"column",
+//     "padding-top":"22px",
+//     "margin-right":"100px"
+//     });
+// });
+
+
+$("#hamburgers").click(function() {
+  if(!$('#hamburgers').hasClass('dropdown')){
+    $('.dropdown').show(1000);
+  }
+  else{
+    console.log('another click');
+    $('.dropdown').hide(1000);
+  }
+});
+
